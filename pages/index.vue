d0d577584d5785df3d2edca36455fdc478f2db09
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-11-18 14:59:36 +1100

    initial server render demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <h1>Hello world!</h1>
-    <NuxtLink to="/about">About page</NuxtLink>
+    <h1>Hello Nuxters! 👋</h1>
+    <p>
+      This page is rendered on the <strong>{{ rendering }}</strong>
+    </p>
+    <p v-if="rendering === 'server'">
+      First load or hard refresh is done on server side.
+    </p>
+    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
+    <ul>
+      <li>Refresh the page for server side rendering.</li>
+      <li>Click the links to see client side rendering.</li>
+    </ul>
+
+    <NuxtLink to="/about">About Page</NuxtLink>
   </div>
 </template>
+<script>
+export default {
+  asyncData() {
+    console.log("process.server", process.server);
+    return {
+      rendering: process.server ? "server" : "client",
+    };
+  },
+};
+</script>
